91522d4bb7945d818e2cdd5c86ba7529e3685d4f
Manuel Araujo <manuelmad1981@gmail.com>
2024-07-31 12:17:22 -0400

    Working on new event template.

--- app/MainView/MainView.js
+++ app/MainView/MainView.js
@@ -1,7 +1,7 @@
 'use client';
 
 import './MainView.css';
-import Header from '../Header/Header';
+//import Header from '../Header/Header';
 
 import { db } from '../firebase/firebase-config';
 import { collection, onSnapshot, getDocs } from "firebase/firestore";
@@ -24,6 +24,26 @@ export default function MainView({
     // Array containing all collections
     const collectionsArrayByField = [alpufCollection, alturitasCollection, machiquesCollection, sanjoseCollection, sanjulianCollection];
 
+    // Array containing all event types
+    const event_types = [
+        {
+            type: '--------',
+            subtypes: ['--------']
+        },
+        {
+            type: 'Perforación',
+            subtypes: ['Original', 'Side Track', 'Re Entry', 'Grass Root']
+        },
+        {
+            type: 'Servicio',
+            subtypes: ['RIBES', 'Conexión Superficial']
+        },
+        {
+            type: 'RA/RC',
+            subtypes: ['Cambio de método', 'Limpieza de fondo', 'Pesca']   
+        }
+    ];
+
     // Function to show new Event Modal
     const showNewEventModal = ()=> {
         setNewEventModalDisplay({display: 'block'});
@@ -31,13 +51,13 @@ export default function MainView({
         const element = document.querySelector('.new-event__modal');
         const offset = 300;
         const bodyRect = document.body.getBoundingClientRect().top;
-        console.log(bodyRect);
+        // console.log(bodyRect);
         const elementRect = element.getBoundingClientRect().top;
-        console.log(elementRect);
+        // console.log(elementRect);
         const elementPosition = elementRect - bodyRect;
-        console.log(elementPosition);
+        // console.log(elementPosition);
         const offsetPosition = elementPosition + offset;
-        console.log(offsetPosition);
+        // console.log(offsetPosition);
         // A little delay while the section is shown and able to go there
         setTimeout(()=> {
             window.scrollTo({
@@ -52,6 +72,7 @@ export default function MainView({
         setNewEventModalDisplay({display: 'none'});
     }
 
+    // Function to add options in the wells select, corresponding to the selected field
     const showWellsField = async (e) => {
         // Clearing wells select
         const well_select = document.getElementById('well_select');
@@ -67,6 +88,11 @@ export default function MainView({
         const ref = await getDocs(col);
         const well = ref.docs;
 
+        const well_deafult_option = document.createElement('option');
+        well_deafult_option.innerText = '--------';
+        well_deafult_option.value = '--------';
+        well_select.appendChild(well_deafult_option);
+
         // Adding an option in the wells select for each well in the collection
         well.forEach(well => {
             const well_option = document.createElement('option');
@@ -74,6 +100,21 @@ export default function MainView({
             well_option.value = well.id;
             well_select.appendChild(well_option);
         })
+    }
+
+    // function to add options in the subtype select, corrsponding to the selected type event
+    const showSubtype = (e) => {
+        const event_subtype_select = document.getElementById('event_subtype_select');
+        event_subtype_select.innerHTML = '';
+        const typeValue = e.target.value;
+        const typeMatch = event_types.find(type => type.type === typeValue);
+        typeMatch.subtypes.forEach(subtype => {
+            const subtype_option = document.createElement('option');
+            subtype_option.innerText = subtype;
+            subtype_option.value = subtype;
+            event_subtype_select.appendChild(subtype_option);
+        });
+
     }
 
     useEffect(()=> {
@@ -212,16 +253,54 @@ export default function MainView({
                 <section style={newEventModalDisplay}>
                     <div className='new-event__modal'>
                         <p>
-                            <label>Elija el campo:</label>
+                            <label htmlFor='field_select'>Seleccione el CAMPO donde se ejecutará el evento:</label>
                             <select onChange={showWellsField} id='field_select'>
 
                             </select>
                         </p>
                         <p>
-                            <label>Elija un pozo:</label>
+                            <label htmlFor='well_select'>Seleccione un POZO del campo seleccionado anteriormente:</label>
                             <select id='well_select'>
 
                             </select>
+                        </p>
+                        <p>
+                            <label htmlFor='event_type_select'>Seleccione el TIPO del evento a ejecutar en el pozo:</label>
+                            <select onChange={showSubtype} id='event_type_select'>
+                                {event_types.map(type => {
+                                    return <option value={type.type} key={type.type}>{type.type}</option>
+                                })}
+                            </select>
+                        </p>
+                        <p>
+                            <label htmlFor='event_subtype_select'>Seleccione el SUBTIPO del evento a ejecutar en el pozo:</label>
+                            <select id='event_subtype_select'>
+
+                            </select>
+                        </p>
+                        <p>
+                            <label htmlFor='goal_formation'>Indique la formación OBJETIVO en el pozo:</label>
+                            <input id='goal_formation'>
+
+                            </input>
+                        </p>
+                        <p>
+                            <label htmlFor='rig_name'>Indique el EQUIPO / TALADRO con que se ejecutarán las actividades:</label>
+                            <input id='rig_name'>
+
+                            </input>
+                        </p>
+                        <p>
+                            <label htmlFor='init_date'>Indique la FECHA DE INICIO del evento:</label>
+                            <input id='init_date'>
+
+                            </input>
+                        </p>
+                        <p>
+                            <label htmlFor='estimated_time'>Indique el TIEMPO ESTIMADO del evento:</label>
+                            <input id='estimated_time' type='number'>
+
+                            </input>
                         </p>
                         <p id='cancel-event-btn__container'>
                             <button onClick={hideNewEventModal} id='cancel_event_btn'>CANCELAR</button>
