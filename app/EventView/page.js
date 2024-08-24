4603857ee91e286cc32afb4d49cec39f47c7a92e
Manuel Araujo <manuelmad1981@gmail.com>
2024-08-24 13:28:27 -0400

    Adding the function to delete reports.

--- app/EventView/page.js
+++ app/EventView/page.js
@@ -10,7 +10,7 @@ import { HeaderContext } from '../context/context';
 
 import { db } from '../firebase/firebase-config';
 import { getAuth, onAuthStateChanged  } from "firebase/auth";
-import { collection, doc, getDoc, getDocs, updateDoc, query, orderBy } from "firebase/firestore";
+import { collection, doc, getDoc, getDocs, updateDoc, query, orderBy, deleteDoc } from "firebase/firestore";
 
 export default function EventView() {
 
@@ -91,7 +91,18 @@ export default function EventView() {
             const a = document.createElement('a');
             const p = document.createElement('p');
             a.innerHTML = `${reportInfo.Tipo} - ${((new Date ((reportInfo['Fecha'].seconds)*1000)).getDate())+1}/${((new Date ((reportInfo['Fecha'].seconds)*1000)).getMonth())+1}/${(new Date ((reportInfo['Fecha'].seconds)*1000)).getFullYear()}.`;
+            const span = document.createElement('span');
+            span.innerHTML = `
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
+                </svg>
+            `;
+            span.onclick = async () => {
+                await deleteDoc(doc(db, `${reportsInEvent}/${report.id}`));
+                window.location.reload(); // I can't find a better way to update the list of events at 'events_container'
+            }
             p.appendChild(a);
+            p.appendChild(span);
             article2.appendChild(p);
             // Event to create a table when a report in the list is clicked
             a.addEventListener('click', ()=> {
